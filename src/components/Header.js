68a0db665ab617b7fa0f3a1d0072cd935b81a092
Jackjack <53937839+CKMISJONAS20@users.noreply.github.com>
2025-05-15 15:11:48 +0800

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header style={{ backgroundColor: '#222', color: 'white', padding: '20px 0' }}>
+      <h1>My Portfolio</h1>
+    </header>
+  );
+}
+
+export default Header;
