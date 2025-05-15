d79669273cee122718486b143018ab4d01e40979
Jackjack <53937839+CKMISJONAS20@users.noreply.github.com>
2025-05-15 15:12:24 +0800

    Create Projects.js

---
+++ src/components/Projects.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Projects() {
+  return (
+    <section>
+      <h2>Projects</h2>
+      <ul>
+        <li>Project One - React App</li>
+        <li>Project Two - API Integration</li>
+      </ul>
+    </section>
+  );
+}
+
+export default Projects;
