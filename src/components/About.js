13d8b1317f3fde087bdd6a3dcf085f2c29c27895
Jackjack <53937839+CKMISJONAS20@users.noreply.github.com>
2025-05-15 15:12:02 +0800

    Create About.js

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <section>
+      <h2>About Me</h2>
+      <p>Hello! I'm a developer passionate about building cool things with code.</p>
+    </section>
+  );
+}
+
+export default About;
